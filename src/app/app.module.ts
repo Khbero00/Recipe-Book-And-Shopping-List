6bde56f4e82c0dc46dc7f123edacc7c37461b3a9
k.baker <K.baker@nextech.com>
2018-09-09 11:12:23 -0400

    Recipe Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './features/recipe-book/recipe-book.component';
+import { RecipeDetailComponent } from './features/recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './features/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './features/recipe-book/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
